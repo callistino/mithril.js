161c478e1b901f47fc6d602f0b675099efe34a07
Reinier Pelayo <reinier.pelayo@peak10.com>
2017-07-05 17:26:19 -0500

    Render components after data available

--- examples/dbmonster/cell/app.js
+++ examples/dbmonster/cell/app.js
@@ -1,88 +1,45 @@
-var renderStage = 0
+"use strict"
+
 perfMonitor.startFPSMonitor()
 perfMonitor.startMemMonitor()
 perfMonitor.initProfiler("render")
 
 var cell = {
 	$cell: true,
-	$type: 'div',
+	id: 'app',
 	_databases: [],
 	$init: function() {
-		requestAnimationFrame(this.update.bind(this))
-		this._databases = ENV.generateData().toArray()
-
-		if (renderStage === 0) {
-			renderStage = 1
-			perfMonitor.startProfile('render')
-		}
+		this._update();
 	},
 	$update: function() {
-		if (renderStage === 1) {
-			renderStage = 0
-			perfMonitor.endProfile('render')
-		}
+		this.querySelector('#tableData').$components = this._databases.map(this._generateTableData)
 	},
-	$components: [
-		{
-			$type: 'table',
-			class: 'table table-striped latest-data',
-			$components: [
-				{
-					$type: 'tbody',
-					$components: [
-						{
-							$type: 'tr',
-							$components: this._databases.map(this._tableData).pop()
-						}
-					]
-				}
-			]
-		}
-	],
-	_tableData: function(db) {
-		return [
-			{
-				$type: 'td',
-				class: 'dbname',
-				$text: db.dbname,
-			},
-			{
-				$type: 'td',
-				class: 'query-count',
-				$text: db.dbname,
-				$components: [
-					{
-						$type: 'span',
-						$text: db.lastSample.nbQueries,
-						class: db.lastSample.countClassName
-					}
-				]
-			},
-		].concat(db.lastSample.topFiveQueries.map(this._topFiveQueries))
+	_update: function() {
+		requestAnimationFrame(this._update)
 
+		perfMonitor.startProfile("render")
+		this._databases = ENV.generateData().toArray()
+		perfMonitor.endProfile("render")
 	},
-	_topFiveQueries: function(q) {
-		return {
-			$type: 'td',
-			class: q.elapsedClassName,
-			$text: q.formatElapsed,
-			$components: [
-				{
-					$type: 'div',
-					class: 'popover left',
-					$components: [
-						{
-							$type: 'div',
-							class: 'popover-content',
-							$text: q.query
-						},
-						{
-							$type: 'div',
-							class: 'arrow',
-						}
-					]
-				}
-			]
-		}
+	$components: [{$type: 'table', class: 'table table-striped latest-data', $components: [
+			{$type: 'tbody', id: 'tableData'}
+		]}
+	],
+	_generateTableData: function(db) {
+		return {$type: 'tr', $components: [
+			{$type: 'td', class: 'dbname', $text: db.dbname},
+			{$type: 'td', class: 'query-count', $components: [
+				{$type: 'span', class: db.lastSample.countClassName, $text: db.lastSample.nbQueries}
+			]},
+			db.lastSample.topFiveQueries.map(function(q) {
+				return {$type: 'td', class: q.elapsedClassName, $text: q.formatElapsed, $components: [
+					{$type: 'div', class: 'popover left', $components: [
+						{$type: 'div', class: 'popover-content', $text: q.query},
+						{$type: 'div', class: 'arrow',}
+					]}
+				]}
+			})
+		]}
+
 	}
 }
